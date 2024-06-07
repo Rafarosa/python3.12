e56555a7dbb00605d60c6f435f4b2818f3e7853f
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2024-06-07 16:28:34 -0300

    Utilitários do Python Dir e Help

---
+++ Secao2_Introdução_a_linguagem_Python/03_dir_help.py
@@ -0,0 +1,6 @@
+""" 
+Dir -> Apresentan todos os atributos/propriedade e funções/métodos disponivel para determinado tipo de dado ou variavel 
+dir(tipo de dado ou variá)
+
+Help -> Apresenta a documentação/ como utilizar os atributos /propriedades e funções/métodos disponiveis para deterninado tipo de dado 
+"""
