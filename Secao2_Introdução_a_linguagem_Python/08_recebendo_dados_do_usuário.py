a3c3b74a54ea2cfb2381379ebe688690c38d93e4
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2024-06-07 17:15:39 -0300

    Método de receber informações no Python

---
+++ Secao2_Introdução_a_linguagem_Python/08_recebendo_dados_do_usuário.py
@@ -0,0 +1,12 @@
+""" 
+08 - Recebendo dados do usuário
+"""
+# Entrada de dados
+nome = input('Qual o seu nome? ')
+idade = input('Qual a sua idade?')
+# processamento
+
+# Saida
+print(f'seja bem vindo(), {nome}')
+print(f'Sua idade é {idade}')
+print(f'Você nasceu em {2024 - int(idade)}')
